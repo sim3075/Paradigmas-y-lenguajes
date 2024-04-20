d414aff48dfbc70001c631a6544a29f91b70f5ad
Juan Felipe Palacio <110949752+jfpa11100@users.noreply.github.com>
2024-04-20 17:32:37 -0500

    Revert "Conexión inicial a la base de datos con modelo básico de carros"

--- booking/models/carSchema.ts
+++
@@ -1,9 +0,0 @@
-import { Types, Schema, model, models } from "mongoose"; 
-
-
-export const carSchema = new Schema({
-    "Placa": { type: String },
-    "Color": { type: String, required: true }
-})
-
-export const carModel = models.Car  || model("Car", carSchema)
