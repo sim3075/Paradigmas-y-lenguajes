f807a60e6bae72fac7c3f0e1de4f5c6a96307c50
Juan Felipe Palacio <110949752+jfpa11100@users.noreply.github.com>
2024-05-06 17:47:41 -0500

    Update page.tsx

--- booking/app/cars/deal/[id]/page.tsx
+++ booking/app/cars/deal/[id]/page.tsx
@@ -4,54 +4,7 @@ import { useRouter } from "next/navigation";
 export default function Deal({ params }: any) {
     const router = useRouter();
     async function createDeal() {
-        try {
-            if (window.confirm("Are you sure you want to book the car?")) {
-                let dealCar = null;
-                await fetch(`${window.location.origin}/api/cars/${params.id}`)
-                    .then((response) => response.json())
-                    .then((car) => {
-                        dealCar = car
-                    });
-                if (dealCar !== null) {
-                    console.log("Deal car stringify:", JSON.stringify(dealCar));
-                    console.log(params.id)
-                    const response = await fetch(
-                        `${window.location.origin}/api/cars/deal`,
-                        {
-                            method: "POST",
-                            body: JSON.stringify({
-                                user: {
-                                    name: "User Name",
-                                    contact: {
-                                        prefix: 57,
-                                        phone: 3013743983,
-                                        email: "example@gmail.com",
-                                    },
-                                },
-                                pickUpDate: "2024-05-31",
-                                dropOffDate: "2024-06-03",
-                                car: dealCar,
-                                price: 400,
-                            }),
-                            headers: {
-                                "Content-Type": "application/json",
-                            },
-                        }
-                    );
-                    if (response.status === 200) {
-                        console.log(
-                            "response json: " + (await response.json())
-                        );
-                        router.push("/cars");
-                        // router.refresh()
-                    }
-                } else {
-                    throw new Error("Couldn´t make deal");
-                }
-            }
-        } catch (error) {
-            console.log(error);
-        }
+        // Logic to create deal (booking)
     }
 
     return (
