b71fc88a7a29e1031d3823efcd6a38c60b3fe1aa
Znorlux <mabe0898@hotmail.com>
2024-05-13 20:10:12 -0500

    Diseño de register y login finalizado con animaciones

--- booking/app/sign-up/[[...sign-up]]/page.tsx
+++ booking/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,21 @@
+import Header from "@/app/components/Header";
+import Transition from "@/app/components/Transition";
 import { SignUp } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="flex justify-center">
-      <SignUp />
-    </div>
+    <Transition>
+      <Header isHome={true} />
+      <div className="absolute inset-0 z-[-1]">
+        <div className="flex h-screen w-1/2">
+          <Image src="/register-bg.jpg" alt="background" fill />
+          <div className="absolute inset-0 bg-gray-950 opacity-50"></div>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <SignUp />
+      </div>
+    </Transition>
   );
 }
