d84b81adad9fdc0c25ffc8fb2917bd13a79a3504
Znorlux <mabe0898@hotmail.com>
2024-05-23 00:55:02 -0500

    Vista de busqueda de carros finalizada

--- booking/app/car-search/page.tsx
+++ booking/app/car-search/page.tsx
@@ -17,14 +17,14 @@ export type carParams = {
 async function SearchPage({ searchParams }: Props) {
   if (!searchParams.location) return notFound();
 
-  console.log(
-    `localhost:3000/api/car/search?origin=${searchParams.location}&brand=${searchParams.brand}`
-  );
-  const results = fetch(
+  //console.log(
+  //  `localhost:3000/api/car/search?origin=${searchParams.location}&brand=${searchParams.brand}`
+  //);
+  const results = await fetch(
     `http://localhost:3000/api/car/search?origin=${searchParams.location}&brand=${searchParams.brand}`
-  ).then(async (res) => {
-    await res.json();
-  });
+  );
+
+  const data = await results.json();
 
   if (!results) return <div>No results...</div>;
 
@@ -47,34 +47,31 @@ async function SearchPage({ searchParams }: Props) {
           {/* Aquí se muestra la lista de resultados de la búsqueda*/}
         </h3>
 
-        {/*<div className="space-y-2 mt-5">
-          {results..map((item, i) => (
+        <div className="space-y-2 mt-5">
+          {data.map((item: any, i: any) => (
             <div
               key={i}
               className="flex space-y-2 justify-between space-x-4 p-5 border rounded-lg"
             >
               <img
-                src={item.url}
+                src={item.photo}
                 alt="image of property"
-                className="h-44 w-44 rounded-lg"
+                className="h-44 w-54 rounded-lg"
               />
 
               <div className="flex flex-1 space-x-5 justify-between">
                 <div>
-                  <Link
-                    href={item.link}
-                    className="font-bold text-blue-500 hover:text-blue-600 hover:underline"
-                  >
-                    {item.title}
-                  </Link>
-                  <p className="text-xs">{item.description}</p>
+                  <p className="text-xl text-bold">
+                    {item.brand} en {item.city}
+                  </p>
+                  <p className="text-sm">{item.description}</p>
                 </div>
 
                 <div className="flex flex-col justify-between">
                   <div className="flex items-start justify-end space-x-2 text-right">
                     <div>
-                      <p className="font-bold">{item.rating_word}</p>
-                      <p className="text-xs">{item.rating_count}</p>
+                      <p className="font-bold">Calificación</p>
+                      <p className="text-xs">Excelente</p>
                     </div>
 
                     <p className="flex items-center justify-center font-bold text-sm w-10 h-10 text-white bg-blue-900 rounded-lg flex-shrink-0">
@@ -83,14 +80,14 @@ async function SearchPage({ searchParams }: Props) {
                   </div>
 
                   <div className="text-right">
-                    <p className="text-xs ">{item.booking_metadata}</p>
+                    <p className="text-lg ">Asientos: {item.seats}</p>
                     <p className="text-2xl font-bold">{item.price}</p>
                   </div>
                 </div>
               </div>
             </div>
           ))}
-        </div>*/}
+        </div>
       </div>
     </section>
   );
